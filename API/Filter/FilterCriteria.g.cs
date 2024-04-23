6a4af1d802c58107ee58a9ee7fb3aa53f2228dd5
Novel Teams Codezen <159769018+novelteams-codezen@users.noreply.github.com>
2024-04-23 19:36:34 +0530

    Upload files from codezen

---
+++ API/Filter/FilterCriteria.g.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClarionHub.Filter
+{
+    public class FilterCriteria
+    {
+        public string PropertyName { get; set; }
+        public string Operator { get; set; }
+        public string Value { get; set; }
+    }
+}
